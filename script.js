ca278805f93f5753ee11f4439a7ef97f9d532ca8
Henrique Lucas <henriquelucas15@outlook.com.br>
2024-03-17 21:51:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('farmacia-card').addEventListener('click', function(event) {
     // Evita o comportamento padrão do formulário que é atualizar a página
     event.preventDefault();
-    window.location.href = `src/farmacia/farmarcia.html`;
+    window.location.href = `src/geral/geral.html`;
 });
