2cbac241bd5b8014713f8697174957c7913727fe
Henrique Lucas <henriquelucas15@outlook.com.br>
2024-03-26 16:01:12 -0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     // Evita o comportamento padrão do formulário que é atualizar a página
     event.preventDefault();
-    window.location.href = `/farmacia/src/paginas/dashboard/dashboard-farmacia.html`;
+    window.location.href = `/src/paginas/dashboard/dashboard-farmacia.html`;
 });
 document.getElementById('farmacia-card').addEventListener('click', function(event) {
     // Evita o comportamento padrão do formulário que é atualizar a página
